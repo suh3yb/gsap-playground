025fe7ee5af5ac8153d1602c8e05366f2a6931ec
Suheyb Coskun <suheyb.c@gmail.com>
2022-01-10 14:09:06 +0100

    add scroll effects to pin time

--- 3. Projects/1.StartingProjects/6. Pin Time/app.js
+++ 3. Projects/1.StartingProjects/6. Pin Time/app.js
@@ -1,14 +1,45 @@
 const src = [
-    "ressources/spartan.svg",
-    "ressources/castle.svg",
-    "ressources/da-vinci.svg",
-    "ressources/airplane.svg",
-  ];
-  
-  const dates = [
-    "-3300 | 476",
-    "476 | 1492",
-    "1492 | 1792",
-    "1792 | AJD",
-  ]
-  
+  'ressources/spartan.svg',
+  'ressources/castle.svg',
+  'ressources/da-vinci.svg',
+  'ressources/airplane.svg',
+]
+
+const dates = ['-3300 | 476', '476 | 1492', '1492 | 1792', '1792 | AJD']
+
+ScrollTrigger.create({
+  trigger: '.sections',
+  pin: '.tooltip',
+  start: 'top 40%',
+  end: 'bottom 55%',
+})
+
+const sections = document.querySelectorAll('.section')
+
+sections.forEach((section, i) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: 'top center',
+    end: 'bottom center',
+    onEnter: () => {
+      gsap.set('.tooltip-img', {
+        attr: {
+          src: src[i],
+        },
+      })
+      gsap.set('.tooltip p', {
+        innerText: dates[i],
+      })
+    },
+    onEnterBack: () => {
+      gsap.set('.tooltip-img', {
+        attr: {
+          src: src[i],
+        },
+      })
+      gsap.set('.tooltip p', {
+        innerText: dates[i],
+      })
+    },
+  })
+})
